66afb46c458c062af21fc3d2969ba0e26ee36dc4
Simplifying-Tech <40490417+patilsushil1001@users.noreply.github.com>
2023-10-07 23:10:15 +0530

    Add files via upload

---
+++ src/main/java/com/stech/SpringBootLoggingApp/SpringBootLoggingAppApplication.java
@@ -0,0 +1,13 @@
+package com.stech.SpringBootLoggingApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLoggingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootLoggingAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stech/SpringBootLoggingApp/SpringBootLoggingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stech.SpringBootLoggingApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootLoggingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
